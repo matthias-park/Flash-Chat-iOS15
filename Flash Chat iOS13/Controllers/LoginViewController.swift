4ace87885939f0835e7cd9cc18cf0ea33ca25362
Junsoo Park <matthiaspark0514@gmail.com>
2021-12-03 16:32:34 -0800

    add Login and sign-out from Firebase with navigation

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,10 +7,24 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+    @IBAction func loginPressed(_ sender: Any) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) {authResult, error in
+                if let e = error {
+                    print(e)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+              
+            }
+        }
+
+    }
 }
